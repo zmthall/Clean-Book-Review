bbd7dd7c834100985167ce56c72534c10555604e
Zack Thallas <zmthall@ZMT-MacBook-Pro.local>
2024-11-05 21:16:29 -0700

    Updated use-case export index

--- use-case/index.js
+++ use-case/index.js
@@ -2,13 +2,23 @@
 import { makeCreateBookReview } from "./createBookReview.js";
 import { makeGetBookReview } from "./getBookReview.js";
 import { makeGetAllBookReviews } from "./getAllBookReviews.js";
+import { makeGetRandomBookReview } from "./getRandomBookReview.js";
+import { makeGetRandomBookReviews } from "./getRandomBookReviews.js";
 import { makeUpdateBookReview } from "./updateBookReview.js";
 import { makeDeleteBookReview } from "./deleteBookReview.js";
+import { makeFilterBookReviews } from "./filterBookReviews.js";
+import { makeSearchBookReviews } from "./searchBookReviews.js";
+import { makeSortBookReviews } from "./sortBookReviews.js";
 
-export {
+export default {
     makeCreateBookReview,
     makeGetBookReview,
     makeGetAllBookReviews,
+    makeGetRandomBookReview,
+    makeGetRandomBookReviews,
     makeUpdateBookReview,
-    makeDeleteBookReview
+    makeDeleteBookReview,
+    makeFilterBookReviews,
+    makeSearchBookReviews,
+    makeSortBookReviews
 };
