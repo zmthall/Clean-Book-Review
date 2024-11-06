6d2d73618a2931597b67609a3f69cd103090fbfe
Zack Thallas <zmthall@ZMT-MacBook-Pro.local>
2024-11-05 18:20:47 -0700

    Decided on how to represent date values in book-review entity

---
+++ frameworks/dbRepository.js
@@ -0,0 +1,18 @@
+
+export const dbRepository = {
+    create: async (review) => {
+
+    },
+    read: async (id) => {
+
+    },
+    readAll: async () => {
+
+    },
+    update: async (id, newData) => {
+
+    },
+    delete: async (id) => {
+
+    }
+}
